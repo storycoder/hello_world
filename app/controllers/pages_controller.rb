9e5ab89cca4d40228f72fa10b540cc77194a3d6a
Storycoder <joseph.margolis@gmail.com>
2015-08-13 16:30:47 -0400

    first commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,8 @@
+class PagesController < ApplicationController
+  def home
+  	@Home = Home.new
+  end
+
+  def about
+  end
+end
---
+++ app/models/home.rb
@@ -0,0 +1,9 @@
+class Home
+	def name
+		name = "Jeb"
+	end
+
+	def birthdate
+		birthdate = Date.new(1910,7,8)
+	end
+end
